63db3febc0f99bba9d4e0bb53c2f17fff3e3c12e
viiniciuspaes <viniciusjosepaes@gmail.com>
2018-01-25 19:48:03 -0300

    [API] api can now load photos objects from the database

--- controllers/photos_controller.py
+++ controllers/photos_controller.py
@@ -6,3 +6,8 @@ def save_photo(photo_object):
         add_photo(photo_object)
     else:
         return None
+
+
+def request_photo_path(file_name):
+    photo = search_photo(file_name)
+    return photo if photo else None
