75bf6fc38a217f8ece67fd7c860295ffb411cb73
viiniciuspaes <viniciusjosepaes@gmail.com>
2018-01-25 19:49:36 -0300

    minor bug-fix

--- controllers/photos_controller.py
+++ controllers/photos_controller.py
@@ -3,7 +3,7 @@ from persistence.photo_dao import search_photo, add_photo
 
 def save_photo(photo_object):
     if not search_photo(photo_object.get_name()):
-        add_photo(photo_object)
+        return add_photo(photo_object)
     else:
         return None
 
