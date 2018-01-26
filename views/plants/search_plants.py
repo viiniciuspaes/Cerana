b0f7b4285ebf9f012a9e781f07982c50d42ecec1
João D'Amorim <joao.damorim@gmail.com>
2018-01-25 23:29:08 -0300

    [Views] [Templates] Retornar planta.

--- views/plants/search_plants.py
+++ views/plants/search_plants.py
@@ -9,17 +9,23 @@ plants = Blueprint('plants', __name__,
                     template_folder='templates')
 
 @plants.route('/search_plants', methods=['GET', 'POST'])
-def search_plants():
+def search_plants(result=None):
     form = SearchPlantForm(csrf_enabled=False)
     if request.method == "POST":
         plant = get_plant(form.scientific_name.data)
         if plant:
-            return '''
-        <form method="post">
-            <p><plant.scientific_name>
-            <p><input type=submit value=Login>
-        </form>
-    '''
+            filo = plant.phylum
+            familia = plant.family
+            return filo, familia
+            #result = plant(request.args['family'])
+            #return render_template('pesquisa.html', result=result)
+            #''' 
+        #<form method="post"> 
+            #<p><plant.scientific_name> 
++
+            #<p><input type=submit value=Login> 
+        #</form> 
+   #''' 
         else:
             return "Não achou!"
     return render_template("pesquisa.html", form=form)
